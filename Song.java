1eba4441015407c8d74e105c76fe39c1017052b0
Ivo Mirchev <166992981+AsakuraRonin@users.noreply.github.com>
2024-11-03 15:57:20 +0100

    Create Song.java

---
+++ Song.java
@@ -0,0 +1,11 @@
+package mk.ukim.finki.wp.lab.model;
+
+import java.util.List;
+
+public class Song {
+    private String trackId;
+    private String title;
+    private String genre;
+    private int releaseYear;
+    private List<Artist> performers;
+}
