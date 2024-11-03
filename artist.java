f8c53b4b1c5fb3616f328c9d3dafe1b9a5a87dcd
Ivo Mirchev <166992981+AsakuraRonin@users.noreply.github.com>
2024-11-03 16:00:42 +0100

    Update artist.java

--- artist.java
+++ artist.java
@@ -5,4 +5,16 @@ public class Artist {
     private String firstName;
     private String lastName;
     private String bio;
+
+    public Artist(Long id, String firstName, String lastName, String bio) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.bio = bio;
+    }
+
+    public Long getId() { return id; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getBio() { return bio; }
 }
