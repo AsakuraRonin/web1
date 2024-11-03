dbd2ac71b43a3c7727892c04f5f0b2cf6071873d
Ivo Mirchev <166992981+AsakuraRonin@users.noreply.github.com>
2024-11-03 16:01:19 +0100

    Update Song.java

--- Song.java
+++ Song.java
@@ -1,5 +1,6 @@
 package mk.ukim.finki.wp.lab.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Song {
@@ -7,5 +8,22 @@ public class Song {
     private String title;
     private String genre;
     private int releaseYear;
-    private List<Artist> performers;
+    private List<Artist> performers = new ArrayList<>();
+
+    public Song(String trackId, String title, String genre, int releaseYear) {
+        this.trackId = trackId;
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+
+    public String getTrackId() { return trackId; }
+    public String getTitle() { return title; }
+    public String getGenre() { return genre; }
+    public int getReleaseYear() { return releaseYear; }
+    public List<Artist> getPerformers() { return performers; }
+
+    public void addPerformer(Artist artist) {
+        this.performers.add(artist);
+    }
 }
