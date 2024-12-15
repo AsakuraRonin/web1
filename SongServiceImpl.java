901644be080d0a40142253cbb3731357ff394cf7
Ivo Mirchev <166992981+AsakuraRonin@users.noreply.github.com>
2024-12-15 21:21:42 +0100

    Update SongServiceImpl.java

--- SongServiceImpl.java
+++ SongServiceImpl.java
@@ -1,16 +1,18 @@
-package mk.ukim.finki.wp.lab.service.impl;
+package mk.ukim.finki.wp.lab.service;
 
-import mk.ukim.finki.wp.lab.model.Artist;
 import mk.ukim.finki.wp.lab.model.Song;
-import mk.ukim.finki.wp.lab.repository.ArtistRepository;
+import mk.ukim.finki.wp.lab.model.Artist;
 import mk.ukim.finki.wp.lab.repository.SongRepository;
-import mk.ukim.finki.wp.lab.service.SongService;
-import java.util.List;
+import mk.ukim.finki.wp.lab.repository.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SongServiceImpl implements SongService {
     private final SongRepository songRepository;
     private final ArtistRepository artistRepository;
 
+    @Autowired
     public SongServiceImpl(SongRepository songRepository, ArtistRepository artistRepository) {
         this.songRepository = songRepository;
         this.artistRepository = artistRepository;
@@ -22,12 +24,13 @@ public class SongServiceImpl implements SongService {
     }
 
     @Override
-    public Song findByTrackId(String trackId) {
-        return songRepository.findByTrackId(trackId);
+    public Song findByTrackId(Long trackId) {
+        return songRepository.findById(trackId).orElse(null);
     }
 
     @Override
-    public Artist addArtistToSong(Artist artist, Song song) {
-        return songRepository.addArtistToSong(artist, song);
+    public void addArtistToSong(Artist artist, Song song) {
+        song.getPerformers().add(artist);
+        songRepository.save(song);
     }
 }
