a42c1bb671bbcd5529603e4c6b93d01604fc7b53
Ivo Mirchev <166992981+AsakuraRonin@users.noreply.github.com>
2024-12-15 21:15:43 +0100

    ArtistRepository.java

--- ArtistRepository.java
+++ ArtistRepository.java
@@ -1,26 +1,7 @@
 package mk.ukim.finki.wp.lab.repository;
 
 import mk.ukim.finki.wp.lab.model.Artist;
-import java.util.List;
-import java.util.Optional;
-import java.util.ArrayList;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class ArtistRepository {
-    private final List<Artist> artists = new ArrayList<>();
-
-    public ArtistRepository() {
-        artists.add(new Artist(1L, "Axl", "Rose", "Guns N' Roses frontman"));
-        artists.add(new Artist(2L, "Jon", "Bon Jovi", "Lead singer of Bon Jovi"));
-        artists.add(new Artist(3L, "David", "Bowie", "Legendary rock artist"));
-        artists.add(new Artist(4L, "Freddie", "Mercury", "Queen vocalist"));
-        artists.add(new Artist(5L, "Elton", "John", "Renowned pianist and singer"));
-    }
-
-    public List<Artist> findAll() {
-        return artists;
-    }
-
-    public Optional<Artist> findById(Long id) {
-        return artists.stream().filter(artist -> artist.getId().equals(id)).findFirst();
-    }
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
 }
