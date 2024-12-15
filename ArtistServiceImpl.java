72e0f9809f24d0cb9b8d3edb416acf36ee7a115b
Ivo Mirchev <166992981+AsakuraRonin@users.noreply.github.com>
2024-12-15 21:21:05 +0100

    Update ArtistServiceImpl.java

--- ArtistServiceImpl.java
+++ ArtistServiceImpl.java
@@ -1,13 +1,16 @@
-package mk.ukim.finki.wp.lab.service.impl;
+package mk.ukim.finki.wp.lab.service;
 
 import mk.ukim.finki.wp.lab.model.Artist;
 import mk.ukim.finki.wp.lab.repository.ArtistRepository;
-import mk.ukim.finki.wp.lab.service.ArtistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class ArtistServiceImpl implements ArtistService {
     private final ArtistRepository artistRepository;
 
+    @Autowired
     public ArtistServiceImpl(ArtistRepository artistRepository) {
         this.artistRepository = artistRepository;
     }
