e0c2c183ae36703d73a2d4ab7f75b777e5518678
Ivo Mirchev <166992981+AsakuraRonin@users.noreply.github.com>
2024-12-15 21:14:09 +0100

    Song.java

--- Song.java
+++ Song.java
@@ -1,29 +1,31 @@
 package mk.ukim.finki.wp.lab.model;
 
-import java.util.ArrayList;
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
 public class Song {
-    private String trackId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     private String genre;
     private int releaseYear;
-    private List<Artist> performers = new ArrayList<>();
 
-    public Song(String trackId, String title, String genre, int releaseYear) {
-        this.trackId = trackId;
+    @ManyToOne
+    private Album album;
+
+    @ManyToMany
+    private List<Artist> performers;
+
+    public Song() { }
+
+    public Song(Long id, String title, String genre, int releaseYear, Album album, List<Artist> performers) {
+        this.id = id;
         this.title = title;
         this.genre = genre;
         this.releaseYear = releaseYear;
-    }
-
-    public String getTrackId() { return trackId; }
-    public String getTitle() { return title; }
-    public String getGenre() { return genre; }
-    public int getReleaseYear() { return releaseYear; }
-    public List<Artist> getPerformers() { return performers; }
-
-    public void addPerformer(Artist artist) {
-        this.performers.add(artist);
+        this.album = album;
+        this.performers = performers;
     }
 }
