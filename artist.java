d1685725d689aeb05bbe835bed3a28ee183969ed
Ivo Mirchev <166992981+AsakuraRonin@users.noreply.github.com>
2024-12-15 21:13:39 +0100

    artist.java

--- artist.java
+++ artist.java
@@ -1,10 +1,20 @@
 package mk.ukim.finki.wp.lab.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     private String bio;
+
+    public Artist() { }
 
     public Artist(Long id, String firstName, String lastName, String bio) {
         this.id = id;
@@ -12,9 +22,4 @@ public class Artist {
         this.lastName = lastName;
         this.bio = bio;
     }
-
-    public Long getId() { return id; }
-    public String getFirstName() { return firstName; }
-    public String getLastName() { return lastName; }
-    public String getBio() { return bio; }
 }
